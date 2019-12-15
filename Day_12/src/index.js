7375fbf5c0098ebcfb189a28f7d1fc457824d0b9
Tatu Paronen <tatu.paronen@houston-inc.com>
2019-12-15 22:46:41 +0200

    Setup for Day 12

---
+++ Day_12/src/index.js
@@ -0,0 +1,1 @@
+require('./runtime').run(__dirname + '/resources/input.txt');
---
+++ Day_12/src/runtime.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const processInput = async (input) => {
+    console.log('input', input);
+};
+
+const run = (filename) => {
+    fs.readFile(filename, 'utf8', async (err, data) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        await processInput(data);
+    });
+};
+
+module.exports.run = run;
